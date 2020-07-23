eb9eb898151295f6984c8dd431c93d2a4f848bc5
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2020-07-23 10:59:30 +0300

    dialogs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="app-wrapper">
+              <Header />
+              <NavBar />
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+                </div>
+          </div>
+      </BrowserRouter>
+
   );
 }
 
