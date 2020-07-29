5e5b3f3f8f1c461239e91f3e771a761aa41d0940
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2020-07-29 17:40:34 +0300

    страница пользователе

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import s from './Users.module.css';
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        props.setUsers([
+            {id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Crystal_Clear_kdm_user_female.svg/1200px-Crystal_Clear_kdm_user_female.svg.png',
+                fullName: 'Evgeniya P.', status: 'I am a boss', location: {city: 'Kharkov', country: 'Ukraine'}, subscribed: true},
+            {id: 2, photoUrl: 'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png',
+                fullName: 'Yaroslav K.', status: 'I am a boss too', location: {city: 'Liman', country: 'Ukraine'}, subscribed: false},
+            {id: 3, photoUrl: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png',
+                fullName: 'Marina M.', status: 'I am a boss too', location: {city: 'Lothovaya', country: 'Ukraine'}, subscribed: true}
+        ]);
+    }
+
+
     return (
         <div>
             {
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,12 +4,7 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
         users: [
-            {id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Crystal_Clear_kdm_user_female.svg/1200px-Crystal_Clear_kdm_user_female.svg.png',
-                fullName: 'Evgeniya P.', status: 'I am a boss', location: {city: 'Kharkov', country: 'Ukraine'}, subscribed: true},
-            {id: 2, photoUrl: 'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png',
-                fullName: 'Yaroslav K.', status: 'I am a boss too', location: {city: 'Liman', country: 'Ukraine'}, subscribed: false},
-            {id: 3, photoUrl: 'https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png',
-                fullName: 'Marina M.', status: 'I am a boss too', location: {city: 'Lothovaya', country: 'Ukraine'}, subscribed: true}
+
         ]
 }
 
