48cb02f765dcbdefe704f4420dc823ae23730ff7
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2022-06-01 17:15:39 +0300

    typification of axios, api, enum, Promise

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import * as axios from "axios";
+import axios from "axios";
 
 const instatce = axios.create({
     withCredentials: true,
@@ -13,39 +13,58 @@ export const usersAPI = {
             return response.data;
         })
     },
-    follow (userId) {
+    follow (userId: number) {
         return  instatce.post(`follow/${userId}`, {})
     },
-    unfollow (userId) {
+    unfollow (userId: number) {
         return  instatce.delete(`follow/${userId}`)
     },
-    getProfile(userId) {
+    getProfile(userId: number) {
         console.warn('Obsolete method. Please use profileAPI object.')
         return profileAPI.getProfile(userId);
     }
 }
 
 export const profileAPI = {
-    getProfile(userId) {
+    getProfile(userId: number) {
         return instatce.get(`profile/${userId}`);
     },
-    getStatus(userId) {
+    getStatus(userId: number) {
         return instatce.get(`profile/status/${userId}`);
     },
-    updateStatus(status) {
+    updateStatus(status: string) {
         return instatce.put(`profile/status`, {status});
     }
 }
 
+export enum ResultCodeEnum {
+    Success = 0,
+    Error = 1
+}
+
+export enum ResultCodeForCaptchaEnum {
+    captchaIsRequired = 10
+}
+
+type MeResponseType = {
+    data: {
+        id: number
+        email: string
+        login: string
+    }
+    resultCode: ResultCodeEnum | ResultCodeForCaptchaEnum
+    messages: Array<string>
+}
+
 export const authAPI = {
     me() {
-        return instatce.get(`auth/me`);
+        return instatce.get<MeResponseType>(`auth/me`).then(res => res.data);
     },
-    login(email, password, rememberMe = false) {
-        return instatce.post(`auth/login`, {email, password, rememberMe});
+    login(email: string, password: string, rememberMe = false) {
+        return instatce.post(`auth/login`, {email, password, rememberMe}).then(res => res.data);
     },
     logout() {
-        return instatce.delete(`auth/login`);
+        return instatce.delete(`auth/login`).then(res => res.data);
     }
 }
 
--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,4 +1,4 @@
-import {authAPI} from "../api/api";
+import {authAPI, ResultCodeEnum} from "../api/api";
 import {stopSubmit} from "redux-form";
 
 const SET_USER_DATA = 'SET_USER_DATA';
@@ -48,8 +48,8 @@ export const setAuthUserData = (userId: number | null, email: string | null, log
 export const getAuthUserData = () => async (dispatch: any) => {
     const response = await authAPI.me();
 
-    if (response.data.resultCode === 0) {
-        let {id, email, login} = response.data.data;
+    if (response.resultCode === ResultCodeEnum.Success) {
+        let {id, email, login} = response.data;
         dispatch(setAuthUserData(id, email, login, true));
     }
 }
@@ -57,10 +57,10 @@ export const getAuthUserData = () => async (dispatch: any) => {
 export const login = (email: string, password: string, rememberMe: boolean) => async (dispatch: any) => {
     const response = await authAPI.login(email, password, rememberMe);
 
-    if (response.data.resultCode === 0)
+    if (response.resultCode === ResultCodeEnum.Success)
         dispatch(getAuthUserData());
     else {
-        let messages = response.data.messages.length > 0 ? response.data.messages[0] : 'Something is wrong!';
+        let messages = response.messages.length > 0 ? response.messages[0] : 'Something is wrong!';
         dispatch(stopSubmit('login', {_error: messages}));
     }
 }
@@ -68,7 +68,7 @@ export const login = (email: string, password: string, rememberMe: boolean) => a
 export const logout = () => async (dispatch: any) => {
     const response = await authAPI.logout();
 
-    if (response.data.resultCode === 0)
+    if (response.resultCode === ResultCodeEnum.Success)
         dispatch(setAuthUserData(null, null, null, false));
 }
 
