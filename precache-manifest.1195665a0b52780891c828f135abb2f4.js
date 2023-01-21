43f505ab06a87a865ae0bbf2be42a3c19ec3b1b1
Evgeniya <popovaevg18@gmail.com>
2023-01-21 17:30:05 +0100

    Updates

---
+++ precache-manifest.1195665a0b52780891c828f135abb2f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4502234b4bba7f76fc149fb3acfb52e3",
+    "url": "/react_social_network/index.html"
+  },
+  {
+    "revision": "a6b319367a84d53cd448",
+    "url": "/react_social_network/static/css/main.0d2ab35c.chunk.css"
+  },
+  {
+    "revision": "fc1bb060cdf5ec3dad02",
+    "url": "/react_social_network/static/js/2.bdf5ff8c.chunk.js"
+  },
+  {
+    "revision": "a6b319367a84d53cd448",
+    "url": "/react_social_network/static/js/main.720ddd6e.chunk.js"
+  },
+  {
+    "revision": "342122978595a9d95ac6",
+    "url": "/react_social_network/static/js/runtime~main.1001fc66.js"
+  },
+  {
+    "revision": "e2dc1ebfae6045c9757c9d21c2e5e32a",
+    "url": "/react_social_network/static/media/avatar.e2dc1ebf.png"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/react_social_network/static/media/loader.9064981b.svg"
+  },
+  {
+    "revision": "a11bfcff062bc0f56d9b172f031882fb",
+    "url": "/react_social_network/static/media/logo.a11bfcff.png"
+  },
+  {
+    "revision": "13ffaa1fd50145780452124caafba945",
+    "url": "/react_social_network/static/media/user_icon.13ffaa1f.png"
+  }
+]);
